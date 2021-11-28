9a0519bfd1a6bee557f6f7849afccef135f50b53
Sra1thirdy <89608130+Sra1thirdy@users.noreply.github.com>
2021-11-27 20:35:44 -0600

    added

--- models/walnut.js
+++ models/walnut.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose") 
-const walnutSchema = mongoose.Schema({ 
- walnut_type: String, 
- weight: Number, 
- cost: Number 
-}) 
- 
-module.exports = mongoose.model("walnut", 
-walnutSchema) 
+const mongoose = require("mongoose")
+const walnutSchema = mongoose.Schema({
+
+walnut_type:{
+    type: String,
+    minLength: 3,
+    maxLength: 100
+},
+
+weight : {
+    type:Number,
+},
+
+cost: {
+    type:Number,
+    min:1,
+    max:500
+}
+})
+module.exports = mongoose.model("walnut",walnutSchema)
--- routes/walnut.js
+++ routes/walnut.js
@@ -7,12 +7,22 @@ router.get('/', walnut_controlers.walnut_view_all_Page);
 
 /* GET detail walnut page */ 
 router.get('/detail', walnut_controlers.walnut_view_one_Page); 
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
 /* GET create walnut page */ 
-router.get('/create', walnut_controlers.walnut_create_Page);
+router.get('/create',secured, walnut_controlers.walnut_create_Page);
 /* GET create update page */ 
-router.get('/update', walnut_controlers.walnut_update_Page); 
+router.get('/update',secured, walnut_controlers.walnut_update_Page); 
 /* GET create walnut page */ 
-router.get('/delete', walnut_controlers.walnut_delete_Page); 
+router.get('/delete',secured, walnut_controlers.walnut_delete_Page); 
  
 
 
